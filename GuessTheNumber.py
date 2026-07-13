e03e15494ae71afb51438ef7691cc67a1d2c0443
tharunkrishnakumar1 <tharunkrishnakumar1@gmail.com>
2026-07-13 21:33:04 +0100

    added funct to generate computer's no. based on chosen user difficulty

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -10,3 +10,14 @@ def introGame():
     time.sleep(1)
     print("I will give you hints if your guess is too high or too low.")
     time.sleep(1)
+
+def numbergen(difficulty):
+    if difficulty == "easy":
+        return np.random.randint(1, 6)
+    if difficulty == "medium":
+        return np.random.randint(1, 21)
+    if difficulty == "hard":
+        return np.random.randint(1, 51)
+    if difficulty == "extreme":
+        return np.random.randint(1, 101)
+    return False
